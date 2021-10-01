0fd85d5fe9035204807c3f1c74ace5e472eb10aa
zeromountain <sonyeongsan@gmail.com>
2021-10-01 23:24:05 +0900

    :tada: client & server directory created

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+var app = express();
+const port = 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+module.exports = app;
