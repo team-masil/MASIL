c2d9ecd68a03dda162f6c6a09784be21bc5891a6
seok <dreaming.seok@gmail.com>
2022-03-07 00:37:37 +0900

    deployment bug fix1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;;
 const cookieParser = require("cookie-parser");
 const cors = require('cors')
 
